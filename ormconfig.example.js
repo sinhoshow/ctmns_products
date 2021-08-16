5c0b39cb7c92d349dad9ed7fcd29b37b081f2b23
Nilson Augusto <n.augusto@hotmail.com.br>
2021-08-16 17:30:28 -0300

    add ormconfig.js

---
+++ ormconfig.example.js
@@ -0,0 +1,28 @@
+const rootDir = process.env.NODE_ENV === "development" ?
+    "src" :
+    "dist"
+
+
+module.exports = {
+    "type": "postgres",
+    "host": "localhost",
+    "port": 5432,
+    "username": "docker",
+    "password": "admin",
+    "database": "ctmns",
+    "migrations": [
+        rootDir + "/database/migrations/*{.ts,.js}"
+    ],
+    "entities": [
+        rootDir + "/modules/**/entities/*{.ts,.js}"
+    ],
+    "cli": {
+        "migrationsDir": rootDir + "/database/migrations"
+    },
+    "seeds": [
+        rootDir + "/database/seeds/*{.ts,.js}"
+    ],
+    "factories": [
+        rootDir + "/modules/**/factories/*{.ts,.js}"
+    ]
+}
