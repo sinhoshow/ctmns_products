a19938f9cc25805edecfb07eefd4ce6cefd6efeb
Nilson Augusto <n.augusto@hotmail.com.br>
2021-08-16 17:58:08 -0300

    update db config

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,7 +6,7 @@ interface IOptions {
 
 getConnectionOptions().then(options => {
     const newOptions = options as IOptions;
-    newOptions.host = 'database';
+    newOptions.host = process.env.NODE_ENV === "development" ? 'database' : 'localhost';
     createConnection({
         ...options,
     });
